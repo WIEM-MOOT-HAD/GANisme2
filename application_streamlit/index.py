c61b7bea235120bd3328a91d378d45940163dafa
WIEM-MOOT-HAD <113610710+WIEM-MOOT-HAD@users.noreply.github.com>
2023-02-11 22:45:43 +0100

    Update index.py

--- application_streamlit/index.py
+++ application_streamlit/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import dash
